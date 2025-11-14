2bdae08b5c8a2238549143a3d555214b5f24f386
kurojiinn <ependihadziev@MacBook-Pro-Ependi.local>
2025-11-14 11:24:46 +0300

    доработка работы с бд

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/Golang-Eskar/subscription-aggregator/internal/database"
 	"log"
+
+	"github.com/Golang-Eskar/subscription-aggregator/internal/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Запуск")
 
+	err1 := godotenv.Load()
+	if err1 != nil {
+		return
+	}
 	err := database.Init()
 	if err != nil {
 		log.Fatal("Ошибка базы данных:", err)
